538f0abaf42a719244a38ac755f1c26d911bc029
jhantai <tylerbroceva123@gmail.com>
2023-11-24 17:43:59 +0600

    second commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from 'react';
 import {useState} from "react";
 import axios from "axios";
 import Login from "./registration/login";
+import './App.css'
+import Alert from '@mui/material/Alert';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+
 
 const App = () => {
   const [login, setLogin] = useState
   ({
-    username: 'codify',
-    password: 'codify'
+    username: '',
+    password: ''
   })
   const authorization = async () => {
     try {
@@ -18,22 +23,34 @@ const App = () => {
 
 
     catch (e) {
-      if (response.status === 401) {
-        alert('верный логин и пароль');
-      } else if (response.status === 401) {
+      if (e.response.status === 200) {
+        alert(<Alert onClose={() => {}}>This is a success alert — check it out!</Alert>
+          <Alert
+              action={
+                  <Button color="inherit" size="small">
+                      UNDO
+                  </Button>
+              }
+          >
+              This is a success alert — check it out!
+          </Alert>);
+
+      } else if (e.response.status === 401) {
         alert('неверный логин и пароль');
-      } else if (response.status === 400) {
+      } else if (e.response.status === 400) {
         alert('сервер не работает');
       } else (
           alert('не коректно ввели ')
       )
+        console.log(e.response)
     }}
-    console.log(e.response)
-  }
-      return (
-          <div>
+
+    return (
+        <div className='box'>
+
             <input
                 onChange={(e) => setLogin(prevState => ({...prevState, username: e.target.value}))}
+
                 type="text"
                 placeholder='Логин'
             />
@@ -43,12 +60,15 @@ const App = () => {
                 placeholder='Пароль'
             />
             <input
+
                 type="button"
                 value='Отправить'
                 onClick={authorization}/>
-          </div>
-      )
-}
+        </div>
+    )
+  }
+
+
 
 
 
